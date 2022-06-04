90663e33144e87f88847cd7fca12e0b7007e04b7
James <jim007.atkinson@hotmail.co.uk>
2022-06-04 14:27:53 +0100

    added more components to the water management page

--- SynopticProject/components/Graph.js
+++ SynopticProject/components/Graph.js
@@ -1,8 +1,9 @@
 import React from 'react';
 import {
-  LineChart,
+  LineChart, ProgressChart,
 } from "react-native-chart-kit";
-import { Dimensions, StyleSheet, View, Text, Image, ImageBackground} from "react-native";
+import { Dimensions, StyleSheet, View, Text, Image, ImageBackground, Button} from "react-native";
+import RNSpeedometer from 'react-native-speedometer'
 const screenWidth = Dimensions.get("window").width;
 
 const chartConfig = {
@@ -16,6 +17,13 @@ const chartConfig = {
   useShadowColorFromDataset: false // optional
 };
 
+const progressBarConfig ={
+  backgroundGradientFrom: "#FFFF",
+  backgroundGradientFromOpacity: 0,
+  backgroundGradientTo: "#FFFF",
+  color: (opacity = 1) => `rgba(52, 149, 235, ${opacity})`,
+}
+
 const data = {
   labels: ["Mon", "Tues", "Wed", "Thurs", "Fri", "Sat", "Sun"],
   datasets: [
@@ -28,43 +36,130 @@ const data = {
   legend: ["Water (L)"] // optional
 };
 
+const phLabels = [
+  {
+    key: 3,
+    name: 'Extremely Acidic',
+    labelColor: '#ebc034',
+    activeBarColor:'#ebc034',
+  },
+  {
+    key: 4,
+    name: 'Very Acidic',
+    labelColor:'#ebeb34',
+    activeBarColor:'#ebeb34',
+  },
+  {
+    key: 5,
+    name: 'Mildy Acidic',
+    labelColor:'#b4eb34',
+    activeBarColor:'#b4eb34',
+  },
+  {
+    key: 6,
+    name: 'Very Little Acid',
+    labelColor:'#6beb34',
+    activeBarColor:'#6beb34',
+  },
+  {
+    key: 7,
+    name: 'Neutral',
+    labelColor:'#3aeb34',
+    activeBarColor:'#3aeb34',
+  },
+  {
+    key: 8,
+    name: 'Very Little Alakali',
+    labelColor:'#34eba5',
+    activeBarColor:'#34eba5',
+  },
+  {
+    key: 9,
+    name: 'Mildly Alkaline',
+    labelColor:'#34d9eb',
+    activeBarColor:'#34d9eb',
+  },
+  {
+    key: 10,
+    name: 'Very Alkaline',
+    labelColor:'#34aeeb',
+    activeBarColor:'#34aeeb',
+  },
+  {
+    key: 11,
+    name: 'Extremely Alkaline',
+    labelColor:'#348feb',
+    activeBarColor:'#348feb',
+  },
+  
+  
+]
+
+const waterQuality = {
+   // optional
+   labels: ["Clarity: "],
+  data: [0.6],
+};
+
 export default function Graph()
 {
   return <View style={styles.container}>
+    
+    <Text style={styles.grey}>Water Management</Text>
+         
     {/* Water capacity glass */}
-    <View style={styles.glassContainer}>
-      <Image source={require("../assets/Water.png")} style={styles.water} />
-      <Image source={require("../assets/Empty_Glass.png")} style={styles.glass} />
+    <View style={styles.borderWrapper}>
+      <View style={styles.glassContainer}>
+        <Image source={require("../assets/Water.png")} style={styles.water} />
+        <Image source={require("../assets/Empty_Glass.png")} style={styles.glass} />
+      </View>
+      <Text style={{color:'grey'}}>1000/1000 L</Text>
     </View>
 
-    <Text style={styles.grey}>Water Management</Text>
-    <LineChart
-      data={data}
-      width={screenWidth}
-      height={220}
-      chartConfig={chartConfig}
-      style={styles.graph}
-    />
+    <View style={styles.borderWrapper}>
+      <LineChart
+        data={data}
+        width={screenWidth}
+        height={220}
+        chartConfig={chartConfig}
+        style={styles.graph}
+      />
+    </View>
+    
+    <View style={styles.gridContainer}>
+      <RNSpeedometer labels={phLabels} minValue={3} maxValue={11} value={7} size={200} wrapperStyle={styles.borderWrapper}></RNSpeedometer>
+      <View style={styles.borderWrapper}>
+        <ProgressChart width={340} height={150} strokeWidth={16} chartConfig={progressBarConfig} radius={52} data={waterQuality} hideLegend={false}></ProgressChart>
+      </View>
+    </View>
+    
+    <Button title="+ Enter data manually"></Button>
+
   </View>
 }
 
 
 const styles = StyleSheet.create({
   container: {
-    marginTop: 50,
+    backgroundColor:'#f2f5f3',
+    display:'flex',
+    justifyContent:'center',
+    alignItems: 'center',
+    flex:1,
+    position:'relative'
   },
   grey: {
     color: 'grey',
     fontSize: 30,
-    textAlign: 'center'
   },
   graph:{
-    marginTop:25,
+    padding:20,
+    marginLeft:10
   },
   glassContainer:{
     width: 200,
     height: 200,
-    overflow: 'hidden'
+    overflow: 'hidden',
   },
   water:{
     position: 'absolute',
@@ -72,12 +167,31 @@ const styles = StyleSheet.create({
     height: 200,
     
     left: 0,
-    top: 0,
+    top: 100,
   },
   glass:{
     position: 'relative',
-    width: 200,
-    height: 201,
+    width: 201,
+    height: 205,
     resizeMode: 'contain'
+  },
+  borderWrapper:{
+    display:'flex',
+    justifyContent:'center',
+    alignItems:'center',
+    border: '1px solid white',
+    width:'auto',
+    height:'auto',
+    minHeight:150,
+    padding: 20,
+    margin:20,
+    borderRadius:20,
+    backgroundColor:'white'
+  },
+  gridContainer:{
+    display:'grid',
+    gridTemplateColumns: 'auto auto auto',
+    padding: 10
   }
+
 });
