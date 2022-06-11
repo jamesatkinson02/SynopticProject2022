d97be7d5d614d6a83837770ddc3218a422480d10
GGS92carmnad <azk20heu@uea.ac.uk>
2022-06-11 19:45:44 +0100

    Added shadow to top bar

--- SynopticProject/src/components/Layout/HamburgerSelector.js
+++ SynopticProject/src/components/Layout/HamburgerSelector.js
@@ -4,7 +4,11 @@ import Icon from 'react-native-vector-icons/FontAwesome';
 import { setGestureState } from 'react-native-reanimated/lib/reanimated2/NativeMethods';
 
 const  HamburgerSelector = ({size, color, handleClick}) => {
-    return  <Icon name="bars" size={size} color={color} style={[shared.hamburgerSelector, { marginTop: Platform.OS === "android" ? StatusBar.currentHeight + shared.hamburgerSelector.marginTop : 0 }]} onPress={handleClick} /> 
+    return (
+        <View style={[shared.shadow, {backgroundColor: 'white'}]}>
+            <Icon name="bars" size={size} color={color} style={[shared.hamburgerSelector, { marginTop: Platform.OS === "android" ? StatusBar.currentHeight + shared.hamburgerSelector.marginTop : 0 }]} onPress={handleClick} /> 
+        </View>
+    );
 }
 
 export default HamburgerSelector;
