ec234414fcc7e18a9074dd5c441e5d950c53bb32
James <39443439+jamesatkinson02@users.noreply.github.com>
2022-06-11 16:49:13 +0100

    Update App.js

--- SynopticProject/App.js
+++ SynopticProject/App.js
@@ -27,21 +27,11 @@ import { shared } from './src/styles/sharedSheet';
 import { useFonts } from 'expo-font';
 
 const Stack = createNativeStackNavigator();
-<<<<<<< HEAD
-
-export default function App() {
-=======
->>>>>>> 0beef3da24291c1ba8285e85df20ccc631cf50ea
 
 export default function App() {
   let {token, setToken} = useToken();
   let [state, setState] = useState(false);
 
-  console.log(token);
-
-<<<<<<< HEAD
-  
-=======
   let [fontsLoaded] = useFonts({
     'Lato-Regular': require('./assets/fonts/Lato-Regular.ttf'),
     'Lato-Light': require('./assets/fonts/Lato-Light.ttf'),
@@ -51,8 +41,6 @@ export default function App() {
   if (!fontsLoaded) {
     return <></>
   }
-
->>>>>>> 0beef3da24291c1ba8285e85df20ccc631cf50ea
   return (
     <NavigationContainer>
       <HamburgerSelector size={30} color={'black'} handleClick={() => setState(true)}></HamburgerSelector>
