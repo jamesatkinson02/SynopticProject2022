773d9459173f35d2b773cb7d8a3de7e8d7eafa21
GGS92carmnad <azk20heu@uea.ac.uk>
2022-06-13 14:14:45 +0100

    Register device backend

--- server/routes/devices.js
+++ server/routes/devices.js
@@ -4,9 +4,40 @@ const db = require('../db');
 
 dotenv.config();
 
-router.route('/content-data').get(async (req, res) => {
+router.route('/register-device').get(async (req, res) => {
   let deviceId = req.body.deviceId;
+  let username = req.body.tokenPayload.username;
 
+  if (deviceId.length == 0) {
+    res.send({ err: "Please enter a device ID" });
+    return;
+  }
+
+  db.query('SELECT * FROM devices WHERE device_id=$1', [deviceId])
+  .then(device => {
+    if (device.rowCount == 0) {
+      res.send({ err: "Device ID not found" });
+      return;
+    }
+
+    let deviceData = device.rows[0];
+
+    if (deviceData.owner != null) {
+      res.send({ err: "Device already registered" });
+      return;
+    }
+
+    db.query('UPDATE devices SET owner=$1 WHERE device_id=$2 RETURNING *', [username, deviceId])
+    .then(updatedDevice => {
+      res.send({ newDevice: updatedDevice.rows[0] });
+    })
+    .catch(err => {
+      res.send({ err: "Database error" });
+    });
+  })
+  .catch(err => {
+    res.send({ err: "Database error" });
+  });
   
 });
 
